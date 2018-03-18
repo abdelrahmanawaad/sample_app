55fb92a7709c177c2a3a3ff5eabe1cadf80f6b75
abdelrahmanabdelhalim <abdelrahman.a.awaad@gmail.com>
2018-03-19 01:25:11 +0200

    Add account activation

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user       = users(:michael)
+    @other_user = users(:archer)
+  end
+
+  end
